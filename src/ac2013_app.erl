a823885565872d9cd77703c4115c571e327d65c5
Yuki Ito <yuki@gnnk.net>
2013-12-26 22:45:48 +0900

    rebar create-app

---
+++ src/ac2013_app.erl
@@ -0,0 +1,16 @@
+-module(ac2013_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    ac2013_sup:start_link().
+
+stop(_State) ->
+    ok.
